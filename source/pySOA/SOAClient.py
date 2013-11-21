c5aebd0ba38cc9a842752f702367d8550cc5b332
Patrick Muehl <pmuehl@student.tgm.ac.at>
2013-11-21 21:03:31 +0100

    Suchfunktion hinzugefügt

--- source/pySOA/SOAClient.py
+++ source/pySOA/SOAClient.py
@@ -1,3 +1,6 @@
 import SOAPpy
+#Connect to the server
 server = SOAPpy.SOAPProxy("http://localhost:8080/")
-print server.hello()
+swort= raw_input("Geben Sie ihr gesuchtes Thema ein: ")
+suchwort= swort.strip()
+print server.search(suchwort)
--- source/pySOA/SOAServer.py
+++ source/pySOA/SOAServer.py
@@ -1,6 +1,22 @@
 import SOAPpy
-def hello():
-    return "Hello World"
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse('wissensdatenbank.xml')
+root = tree.getroot()
+def search(suchwort):
+    for tags in root.findall('eintrag'):
+        titel = tags.find('titel').text
+        inhalt = tags.find('inhalt').text
+        if titel.strip() == suchwort:
+            return inhalt.strip()
+        else:
+            return "Kein Eintrag gefunden"
+            
+def insert(element, inhalt):
+    return ""    
+    
 server = SOAPpy.SOAPServer(("localhost", 8080))
-server.registerFunction(hello)
+server.registerFunction(search)
 server.serve_forever()
+
